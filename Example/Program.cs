f5650e866abf68bb3eaeea3d26c44d268fd92791
Cato <catodekruif@gmail.com>
2022-06-26 17:31:18 +0200

    Initial commit

---
+++ Example/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Example
+{
+    internal class Program
+    {
+        public static int live_function()
+        {
+            int a = 3;
+            int b = 4;
+            int c = a + b;
+            return c;
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(live_function());
+        }
+    }
+}
